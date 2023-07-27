d016417499e5009031b89928e1e714d550fe5e83
givra <132878206+givra@users.noreply.github.com>
2023-07-27 15:46:28 +0200

    Add files via upload

--- Simulazione2.C
+++ Simulazione2.C
@@ -3,8 +3,16 @@
 #include <iostream>
 #include <fstream>
 #include <math.h>
+
+#include "TFile.h"
 #include "TTree.h"
 #include "TBranch.h"
+
+#include "Punto.h"
+#include "Punto2.h"
+#include "TClonesArray.h"
+#include "Riostream.h"
+#include "TRandom3.h"
 #include "Vertex.h"
 #include "TracciaMC.h"
 #include "Multis.h"
@@ -16,80 +24,73 @@ using namespace std;
 
 void Simulazione2(){
 	
-	/*unsigned int seed;
+  TStopwatch time;
 	
-	cout << " inserire un seed: " << endl;
-	cin >> seed;
-	
- gRandom->SetSeed(seed);*/
-	
- bool multipleScatt = 0;		// 0 = false, 1 = true
- cout << " attivare multiple scattering? " << endl;
- cin >> multipleScatt;
+ bool multipleScatt = 1;		// 0 = false, 1 = true
  
-  /* ______________grafico 3d intersezioni_______________
-		  TCanvas *c1=new TCanvas("c1","c1",800,600);
-		  
-		  TH3F* h3 = new TH3F("h3", "Simulazione Detector", 16, -8, 8, 16, -8, 8, 28, -14, 14);
-		  h3->Draw(); 
-		  h3->GetXaxis()->SetTitle("X");
-		  h3->GetYaxis()->SetTitle("Y");
-		  h3->GetZaxis()->SetTitle("Z");
-		*/  
-    TFile hfile("htree.root","RECREATE");
+  // ______________grafico 3d intersezioni_______________
+		
+	TFile hfile2("htree2.root","RECREATE");	  
+        TTree *tree2 = new TTree("T2","TTree con 2 branches");
+        
+        int numeroeventi = 1000;
+        
+        TClonesArray *ptrhits1 = new TClonesArray("Punto2",numeroeventi);
+        TClonesArray &hits1 = *ptrhits1;
+        
+        TClonesArray *ptrhits2 = new TClonesArray("Punto2",numeroeventi);
+        TClonesArray &hits2 = *ptrhits2;
+        
+        TClonesArray *ptrhits3 = new TClonesArray("Punto2",numeroeventi);
+        TClonesArray &hits3 = *ptrhits3;
+        
+      
+        
+        ofstream fileout1("Zsim.txt"); //forse questo diventa inutile perchè ho già una foglia del tree con la Z del vertice
+		 
+		   typedef struct{
+                   float X,Y,Z;
+                   int mult;} VTX;
+    
+    
+                   static VTX point;
+		 
+				   tree2->Branch("VertMult",&point.X,"X/F:Y:Z:mult/I");
+                   tree2->Branch("HitsPrimo",&ptrhits1);
+                   tree2->Branch("HitsSecondo",&ptrhits2);
+                   tree2->Branch("HitsTerzo",&ptrhits3);
+                   
+                   vector<float> coordinate, intersez;
+                   Vertex vertice0;
+                   
+              
+	for(int evento = 0; evento < numeroeventi; evento++){
+	
 
-	for(int evento = 0; evento < 1; evento++){
+		    vertice0.NewVertex();
+		 		 
+		     point.mult = vertice0.GetMolteplicity();
+                     point.X = vertice0.GetX();
+                     point.Y = vertice0.GetY();
+                     point.Z = vertice0.GetZ();
+
+		 
 		
-		 Vertex vertice0;
+		 fileout1 <<point.Z<<endl ;
 		 
-		 vector<float> coordinate = vertice0.GetCoordinate();
-		 
-		 int molteplicità = vertice0.GetMolteplicity();
-		 vector<float> intersez;
-		 
-		 float x2,y2,z2,x3,y3,z3;
-		 
-        //ofstream fileout("intersez_simulate.txt");		// creo file output
-        //ofstream fileout1("Zsim.txt");		 
-		  
-		  
-		 //TGraph *g = new TGraph(1 + molteplicità*3);
-		 //TPolyMarker3D *g = new TPolyMarker3D(1 + molteplicità*3);
-		 
-		 //TPolyLine3D *traccie[molteplicità];
-		 //for(int s=0; s<=molteplicità; s++) traccie[s] = new TPolyLine3D(4);
-		 
-		 
-		 //g->SetPoint(0, coordinate[0], coordinate[1], coordinate[2]);
-		 
-		TTree* tree(NULL);
-	    tree = new TTree("T","TREEPROVA");
-	    
-		//tree->Branch("Zsim", &coordinate[2], "coordinate[2]/D");
-	    tree->Branch("x2", &x2, "x2/D");
-	    tree->Branch("y2", &y2, "y2/D");
-	    tree->Branch("z2", &z2, "z2/D");
-	    tree->Branch("x3", &x3, "x3/D");
-	    tree->Branch("y3", &y3, "y3/D");
-	    tree->Branch("z3", &z3, "z3/D");
-	    
-		
-		 cout<<"Molteplicità: "<<molteplicità<<endl;
-		 cout << " Z vertice "<< coordinate[2] << endl;
-		 //fileout1 << coordinate[2] << endl ;
 		 
 		  // __________creo oggetto Multis per il Multiple Scattering____________
 		  
 		  Multis catter;
 		 
 		 // __________calcolo intersezioni su ogni layer____________
-		 
+		
 		 int i = 0;
-		 do{
-		 //for(int i=0; i<=molteplicità; i++){ 
 		 
-			 //traccie[i] -> SetPoint(0, coordinate[0], coordinate[1], coordinate[2]);
-			 TracciaMC tr(0.,0.,coordinate);
+		 do{
+		 
+		
+			 TracciaMC tr(0.,0.,point.X,point.Y,point.Z);
 			 
 			 tr.SetDistribEta(); 
 			 tr.SetDistribPhi();
@@ -100,73 +101,53 @@ void Simulazione2(){
 			 intersez = tr.intersezione(3);
 			 if(intersez[2] < -27/2 || intersez[2] > 27/2) continue;
 			 
-			 /*
-			 // ________intersez layer 1 + multiple scatt ___________
-			 intersez = tr.intersezione(1);
-			 traccie[i]->SetPoint(1, intersez[0], intersez[1], intersez[2]);
-			 
-			 Multis catter1;
-			 catter1.VarioAngolo(tr);
-			 tr.SetOrigine(intersez);
-			 
-			 fout << intersez[0] << " " << intersez[1] << " " << intersez[2] << "	|	";
-			 
-			 // _________intersez layer 2 + multiple scatt___________
-			 intersez = tr.intersezione(2);
-			 traccie[i]->SetPoint(2, intersez[0], intersez[1], intersez[2]);
-
-			 Multis catter2;
-			 catter2.VarioAngolo(tr);
-			 tr.SetOrigine(intersez);
-			 
-			 fout << intersez[0] << " " << intersez[1] << " " << intersez[2] << "	|	";
-			 
-			 // _________intersez layer 3 + multiple scatt___________
-			 intersez = tr.intersezione(3);
-			 traccie[i]->SetPoint(3, intersez[0], intersez[1], intersez[2]);
-			 fout << intersez[0] << " " << intersez[1] << " " << intersez[2] << "\n";
-			 */
 			 
 			 for(int t=0; t<3; t++){
 			 
-				intersez = tr.intersezione(t+1);
-				//traccie[i]->SetPoint(t+1, intersez[0], intersez[1], intersez[2]);
-				
+				intersez = tr.intersezione(t+1);										
 				catter.NuoviAngoli(multipleScatt);
 				catter.VarioAngolo(tr);
 				tr.SetOrigine(intersez);
-				//fileout << intersez[0] << " " << intersez[1] << " " << intersez[2] << "		";
-					if(t == 1){
-						x2 = intersez[0];
-						y2 = intersez[1];
-						z2 = intersez[2];
-					
-					}
-					else if(t == 2){
-						x3 = intersez[0];
-						y3 = intersez[1];
-						z3 = intersez[2];
-					
-				    }
-				}
 				
-				//fileout<<endl;
-			 cout << "x2: " << x2 << " y2: " << y2 << " z2: " << z2 << endl;
-			 cout << "x3: " << x3 << " y3: " << y3 << " z3: " << z3 << endl;
-			// traccie[i]->Draw();
-			tree->Fill();
+				Smearing ringo;
+				ringo.smearZ(intersez[2]);
+                                ringo.smearPhi(intersez[0], intersez[1]);
+				
+				if(t==0) new(hits1[i])Punto2(ringo.GetXrec(),ringo.GetYrec(),ringo.GetZrec(), ringo.GetPhirec());
+				if(t==1) new(hits2[i])Punto2(ringo.GetXrec(),ringo.GetYrec(),ringo.GetZrec(), ringo.GetPhirec());
+				if(t==2) new(hits3[i])Punto2(ringo.GetXrec(),ringo.GetYrec(),ringo.GetZrec(), ringo.GetPhirec());
+				
+				}			
 			
 			 i++;
 			 
-		 }while( i < molteplicità);
+		 }while( i < point.mult);
+               
+               /*  // Debug
+    printf("Evento %d - moltepl: %d\n",evento,point.mult);
+    printf("x= %f ; y= %f; z= %f \n",point.X,point.Y,point.Z);
+    printf("Entries nel TClonesArray: %d\n",ptrhits1->GetEntries());
+    for (int j=0; j<hits1.GetEntries(); j++){
+      Punto *tst=(Punto*)ptrhits1->At(j);
+      cout<<"int primo layer "<<j<<") x, y, z = "<<tst->GetX()<<"; "<<tst->GetY()<<"; "<<tst->GetZ()<<endl;}*/
+    
+    // fine del debug
 		 
-         //fileout.close();
-		 //fileout1.close();      
-
+		 tree2->Fill();
+                 ptrhits1->Clear();
+                 ptrhits2->Clear();
+                 ptrhits3->Clear();
 		 }
-		 //}
-		hfile.Write();		// svuota il buffer di qualsiasi cosa contenga
-		hfile.Close();
-		 //c1->Update(); 
-	//}
+		
+		
+		 fileout1.close();
+		   // Save all objects in this file
+                 hfile2.Write();
+
+                // Close the file. 
+                hfile2.Close();
+                 
+                  double TT = time.CpuTime();	
+  cout<<"Il tempo impiegato dalla CPU è "<<TT<<" s"<<endl;
+	
  }
