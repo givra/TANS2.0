a7d064e4004334488e37e2bd4de438d85a89a9fa
davidezeta00 <132901193+davidezeta00@users.noreply.github.com>
2023-08-02 16:10:35 +0200

    Add files via upload

--- Simulazione_detector/Ricostruzione2.C
+++ Simulazione_detector/Ricostruzione2.C
@@ -1,4 +1,3 @@
-
 #include <iostream>
 #include <fstream>
 #include <math.h>
@@ -26,28 +25,21 @@ void Ricostruzione2(){
 	TStopwatch time;
 	TH1D* hist2 = new TH1D ("h2","zreconstructed versione2" , 80, -14., 14.);
 	
-
-	//ifstream fin("intersez_simulate.txt");
-	//ifstream fin1("intersez_simulate.txt");
 	
-	//ifstream filein1("Zvertex.txt");
-	
-	//ofstream fout("intersez_rec.txt");
-	
-	        float X2, Y2, Z2; //coordinate intersezione secondo layer
+	        float X2, Y2, Z2, Phi2; //coordinate intersezione secondo layer
 			
-			float X3, Y3, Z3;   //coordinate intersezione terzo layer
+		float X3, Y3, Z3, Phi3;   //coordinate intersezione terzo layer
 			
-			float C[3];   //coefficienti retta
-			float t;      //parametro retta
+		float C[3];   //coefficienti retta
+		float t;      //parametro retta
 				
-			float R2 = 4.;
-			float R3 = 7.;
+		float R2 = 4.;
+		float R3 = 7.;
 			
-			float Phi, Theta;
+		float Phi, Theta;
 			
-			float differenza = 0;
-			float Zsim1;
+		float differenza = 0;
+		float Zsim1;
 	        float Zrec1, Zrec;
 			
 			
@@ -71,61 +63,42 @@ void Ricostruzione2(){
   
   // Dichiarazione TClonesArray
   //TClonesArray *hits1 = new TClonesArray("Punto2",numeroeventi);
-  TClonesArray *hits2 = new TClonesArray("Punto2",numeroeventi);
+  TClonesArray *hits2 = new TClonesArray("Punto2",100);
   TClonesArray &ptrhits2 = *hits2;									// per qualche motivo serve l'alias per poi riempirlo
-  TClonesArray *hits3 = new TClonesArray("Punto2",numeroeventi);
+  TClonesArray *hits3 = new TClonesArray("Punto2",100);
   TClonesArray &ptrhits3 = *hits3;
   
   // Definizione degli indirizzi per la lettura dei dati su ttree
   b1->SetAddress(&point.X);
- // b2->SetAddress(&hits1);
+ //b2->SetAddress(&hits1);
   b3->SetAddress(&hits2);
   b4->SetAddress(&hits3);
 
-  numeroeventi = tree2->GetEntries();
   
-  TClonesArray *hitsgood2 = new TClonesArray("Punto2",numeroeventi);
+  TClonesArray *hitsgood2 = new TClonesArray("Punto2",100);
   TClonesArray &ptrhitsgood2 = *hitsgood2;
-  TClonesArray *hitsgood3 = new TClonesArray("Punto2",numeroeventi);
+  TClonesArray *hitsgood3 = new TClonesArray("Punto2",100);
   TClonesArray &ptrhitsgood3 = *hitsgood3;
-  //TFile *dati_rec = new TFile("dati_rec.root","recreate");
-  //TTree *tree2rec = new TTree("T2rec","TTree ricostruzione");
-			
-	// arrays contenenti Z ricostruite e simulate
-	float Zvec[numeroeventi];
-	float Zsim[numeroeventi];
+
+	
+  numeroeventi = tree2->GetEntries();		
+  // arrays contenenti Z ricostruite e simulate
+  float Zvec[numeroeventi];
+  float Zsim[numeroeventi];
 
 
- 
-//---------------------------------------------------------------------
-   //ofstream fileout1("Zrec.txt");
-   /* float mat[81][4][2];
-    
-    TClonesArray *ptrhitsgood2 = new TClonesArray("Punto2",numeroeventi);
-    TClonesArray &hitsgood2 = *ptrhitsgood2;
-        
-    TClonesArray *ptrhitsgood3 = new TClonesArray("Punto2",numeroeventi);
-    TClonesArray &hitsgood3 = *ptrhitsgood3;
-    
-    tree->Branch("GoodHitsSecondo",&ptrhitsgood2);
-    tree->Branch("GoodHitsTerzo",&ptrhitsgood3);*/
   
   // loop sugli ingressi nel TTree
   for(int ev=0;ev<tree2->GetEntries();ev++){
   
-       //ofstream fout1("dati_tracklets.txt");
-	//ifstream fin2("dati_tracklets.txt");
-
-  
     tree2->GetEvent(ev);
-   // cout<<"Evento "<<ev<<"; Molteplicita= "<<point.mult<<endl;
+    cout<<"Evento "<<ev<<"; Molteplicita= "<<point.mult<<endl;
    // cout<<"X,Y,Z = "<<point.X<<"; "<<point.Y<<"; "<<point.Z<<endl;
     
    Zsim[ev] = point.Z;		// riempio il vector coi dati simulati
 
-         
-          //int numerocoppie = 0;
-        // cout << " molteplicità " << point.mult;
+         //int numerocoppie = 0;
+        
 			for(int a = 0; a < point.mult; a++){
 			     Punto2 *tst2;
 			     tst2=(Punto2*)hits2->At(a);
@@ -142,22 +115,16 @@ void Ricostruzione2(){
 					if(abs(deltaPhi) < 0.01){
 						
 						
-						//		X2	|	Y2	|	Z2	|	X3	|	Y3	|	Z3
-						new(ptrhitsgood2[a])Punto2(tst2->GetX(),tst2->GetY(),tst2->GetZ(),tst2->GetPhi());
-						new(ptrhitsgood3[b])Punto2(tst3->GetX(),tst3->GetY(),tst3->GetZ(),tst3->GetPhi());
-						//fout1 << tst2->GetX() << " " << tst2->GetY() << " " << tst2->GetZ() << " "<< tst3->GetX() << " " << tst3->GetY() << " " << tst3->GetZ() << " "<< endl;
+						new(ptrhitsgood2[a])Punto2(tst2->GetPhi(), tst2->GetZ(), tst2->Getnum());
+						new(ptrhitsgood3[b])Punto2(tst3->GetPhi(), tst3->GetZ(), tst3->Getnum());
 						
-						//numerocoppie++;
 					}
 					else continue;
 					
 				}
-				//tree2rec->Fill();
+				
 			}
-			//dati_rec.Write();
-			//dati_rec->Close();
 			
-			//cout << " numero coppie " << numerocoppie << " entries L2 " << hits2->GetEntries() << " entries L3 " << hits3->GetEntries() << endl;
 				
 			// __________tentativo ricostruzione vertice__________
 			// eq retta passante per due punti A(x2,y2,z2) e B(x3,y3,z3)
@@ -167,34 +134,26 @@ void Ricostruzione2(){
 			// noi vogliamo solo la z
 			// impongo y=0 e x=0, trovo quindi due valori di t e li medio tra loro (x3/c1 e y3/c2)
 			
-	*/			
-			for(int e=0; e<hitsgood2->GetEntries(); e++)
+				cout<<ptrhitsgood2.GetEntries()<<endl;
+			for(int e=0; e<ptrhitsgood2.GetEntries(); e++)
 			{
 				Punto2 *tstgood2;
 				Punto2 *tstgood3;
 				tstgood2=(Punto2*)hitsgood2->At(e);
 				tstgood3=(Punto2*)hitsgood3->At(e);
 				
-				X2 = tstgood2->GetX();
-				Y2 = tstgood2->GetY();
+				Phi2 = tstgood2->GetPhi();
+				X2 = R2*TMath::Cos(Phi2);
+				Y2 = R2*TMath::Sin(Phi2);
 				Z2 = tstgood2->GetZ();
 				
-				X3 = tstgood3->GetX();
-				Y3 = tstgood3->GetY();
+				Phi3 = tstgood3->GetPhi();
+				X3 = R3*TMath::Cos(Phi3);
+				Y3 = R3*TMath::Sin(Phi3);
 				Z3 = tstgood3->GetZ();
 				
-				
-				/* //prendo le coppie buone leggendo il file dati_tracklets
-				 fin2 >> X2;
-				 fin2 >> Y2;
-				 fin2 >> Z2;
-				 
-				 
-				 fin2 >> X3;
-				 fin2 >> Y3;
-				 fin2 >> Z3;
-				
-				 */
+								
+				//cout<<" label 2 "<<tstgood2->Getnum()<<"  "<<"label 3 "<<tstgood3->Getnum()<<endl;
 			        //calcolo Phi e Theta della retta passante tra i due punti
 				 if(X3 < X2){
 					Phi = TMath::ATan((Y3-Y2)/(X3-X2)) + TMath::Pi();
@@ -225,28 +184,19 @@ void Ricostruzione2(){
                 
 		
                 hist2->Reset("ICESM");
-                
-                
-		//fin2.close();
-		//fout1.close();
-	
+                ptrhitsgood2.Clear();
+	        ptrhitsgood3.Clear();
 		}
 	
 		
 		delete hist2;
-			
-	 
-		
-		 //fileout1.close();
+					 
 		 hfile2.Close();
-		// dati_rec->Close();
-		
+			
 		 //creo grafico Ztrue-Zrec, avrò numeroeventi dati
 		 TCanvas *c1=new TCanvas("c1","c1",800,600);
 		 TH1D* hist1 = new TH1D ("h2","Ztrue-Zrec" , 100, -1, 1);	
-	         
-	         
-		 
+	         		 
 		 for(int u = 0; u < numeroeventi; u++){
 		 
 		 Zsim1 = Zsim[u];
