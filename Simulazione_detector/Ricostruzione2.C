35d92d8a359ea407dae5f6bc81350d569292fa35
davidezeta00 <132901193+davidezeta00@users.noreply.github.com>
2023-08-03 16:25:32 +0200

    Add files via upload

--- Simulazione_detector/Ricostruzione2.C
+++ Simulazione_detector/Ricostruzione2.C
@@ -15,210 +15,194 @@
 
 using namespace std;
 
-//float T (float x3, float y3, float C[3]);
-
-
-
 void Ricostruzione2(){
 	
 	int numeroeventi;
+	
 	TStopwatch time;
-	TH1D* hist2 = new TH1D ("h2","zreconstructed versione2" , 80, -14., 14.);
 	
+	TH1D* hist2 = new TH1D ("h2","Zreconstructed versione2" , 80, -14., 14.);
 	
-	        float X2, Y2, Z2, Phi2; //coordinate intersezione secondo layer
+	int iter; //iteratore
+	float deltaPhi;
+	
+        float X2, Y2, Z2, Phi2; //coordinate intersezione secondo layer
+        float X3, Y3, Z3, Phi3;   //coordinate intersezione terzo layer
 			
-		float X3, Y3, Z3, Phi3;   //coordinate intersezione terzo layer
-			
-		float C[3];   //coefficienti retta
-		float t;      //parametro retta
+	float C[3];   //coefficienti retta
+	float t;      //parametro retta
 				
-		float R2 = 4.;
-		float R3 = 7.;
+	float R2 = 4.;
+	float R3 = 7.;
 			
-		float Phi, Theta;
+	float Phi, Theta;
 			
-		float differenza = 0;
-		float Zsim1;
-	        float Zrec1, Zrec;
+	float differenza = 0;
+	float Zsim1;
+	float Zrec1, Zrec2;
 			
-			
-		 
- //----------------------------------------lettura del tree		
-
-  // definizione struct
-  typedef struct {
-    float X,Y,Z;
-    int mult;} VTX;
-  static VTX point;
-  //Apertura file di input
-  TFile hfile2("htree2.root");
+        //definizione struct
+        typedef struct {
+           float X,Y,Z;
+           int mult;} VTX;
+        static VTX point;
   
-  //Lettura TTree  e branch
-  TTree *tree2 = (TTree*)hfile2.Get("T2");
-  TBranch *b1=tree2->GetBranch("VertMult");
-  TBranch *b2=tree2->GetBranch("HitsPrimo");
-  TBranch *b3=tree2->GetBranch("HitsSecondo");
-  TBranch *b4=tree2->GetBranch("HitsTerzo");
+        //apertura file di input
+        TFile hfile2("htree2.root");
   
-  // Dichiarazione TClonesArray
-  //TClonesArray *hits1 = new TClonesArray("Punto2",numeroeventi);
-  TClonesArray *hits2 = new TClonesArray("Punto2",100);
-  TClonesArray &ptrhits2 = *hits2;									// per qualche motivo serve l'alias per poi riempirlo
-  TClonesArray *hits3 = new TClonesArray("Punto2",100);
-  TClonesArray &ptrhits3 = *hits3;
+        //lettura TTree  e branch
+        TTree *tree2 = (TTree*)hfile2.Get("T2");
+        TBranch *b1=tree2->GetBranch("VertMult");
+        TBranch *b2=tree2->GetBranch("HitsPrimo");
+        TBranch *b3=tree2->GetBranch("HitsSecondo");
+        TBranch *b4=tree2->GetBranch("HitsTerzo");
   
-  // Definizione degli indirizzi per la lettura dei dati su ttree
-  b1->SetAddress(&point.X);
- //b2->SetAddress(&hits1);
-  b3->SetAddress(&hits2);
-  b4->SetAddress(&hits3);
-
+        numeroeventi = tree2->GetEntries(); //acquisico informazione sul numero di eventi nel mio detector
+        	
+	//arrays contenenti Z ricostruite e simulate
+        float Zrec[numeroeventi];
+        float Zsim[numeroeventi];	
   
-  TClonesArray *hitsgood2 = new TClonesArray("Punto2",100);
-  TClonesArray &ptrhitsgood2 = *hitsgood2;
-  TClonesArray *hitsgood3 = new TClonesArray("Punto2",100);
-  TClonesArray &ptrhitsgood3 = *hitsgood3;
-
+        //dichiarazione TClonesArray
+        //TClonesArray *hits1 = new TClonesArray("Punto2",numeroeventi); //per la ricostruzione non usiamo il primo layer
+        TClonesArray *hits2 = new TClonesArray("Punto2",100);
+        TClonesArray &ptrhits2 = *hits2; 
+      
+        TClonesArray *hits3 = new TClonesArray("Punto2",100);
+        TClonesArray &ptrhits3 = *hits3;
+  
+        //definizione degli indirizzi per la lettura dei dati su ttree
+        b1->SetAddress(&point.X);
+        //b2->SetAddress(&hits1); //relativo al primo layer
+        b3->SetAddress(&hits2);
+        b4->SetAddress(&hits3);
+  
+        //arrays in cui salvo le buone combinazioni di hits
+        TClonesArray *hitsgood2 = new TClonesArray("Punto2",100);
+        TClonesArray &ptrhitsgood2 = *hitsgood2;
+      
+        TClonesArray *hitsgood3 = new TClonesArray("Punto2",100);
+        TClonesArray &ptrhitsgood3 = *hitsgood3;
 	
-  numeroeventi = tree2->GetEntries();		
-  // arrays contenenti Z ricostruite e simulate
-  float Zvec[numeroeventi];
-  float Zsim[numeroeventi];
+  	// loop sugli ingressi nel TTree
+        for(int ev=0;ev<numeroeventi;ev++){
+                
+                iter = 0;
+                
+                tree2->GetEvent(ev);
+                //cout<<"Evento "<<ev<<"; Molteplicita= "<<point.mult<<endl; 
+                // cout<<"X,Y,Z = "<<point.X<<"; "<<point.Y<<"; "<<point.Z<<endl;
+                    
+                Zsim[ev] = point.Z; //riempio il vector coi dati simulati
 
-
-  
-  // loop sugli ingressi nel TTree
-  for(int ev=0;ev<tree2->GetEntries();ev++){
-  
-    tree2->GetEvent(ev);
-    cout<<"Evento "<<ev<<"; Molteplicita= "<<point.mult;
-   // cout<<"X,Y,Z = "<<point.X<<"; "<<point.Y<<"; "<<point.Z<<endl;
-    int iter = 0;
-    
-   Zsim[ev] = point.Z;		// riempio il vector coi dati simulati
-
-         //int numerocoppie = 0;
-        
-			for(int a = 0; a < point.mult; a++){
-			     Punto2 *tst2;
-			     tst2=(Punto2*)hits2->At(a);
-				 
-				 //hits2->Clear();
-				 //hits3->Clear();
-				for(int b = 0; b < point.mult; b++){
-				Punto2 *tst3;
-				tst3=(Punto2*)hits3->At(b);
+                for(int a = 0; a < point.mult; a++){
+			     
+			  Punto2 *tst2;
+			  tst2=(Punto2*)hits2->At(a);
 				
-				//cout<< " " << tst2->GetPhi()<<"        "<<tst3->GetPhi()<<endl;
-
-					float deltaPhi = tst2->GetPhi() - tst3->GetPhi();
-					if(abs(deltaPhi) < 0.01){
-						
+			  if(tst2->Getnum()!=-1){
+			      for(int b = 0; b < point.mult; b++){
+				  
+				  Punto2 *tst3;
+				  tst3=(Punto2*)hits3->At(b);
+				  if(tst3->Getnum()!=-1){
+					deltaPhi = tst2->GetPhi() - tst3->GetPhi();
+					if(abs(deltaPhi) < 0.01){						
 						new(ptrhitsgood2[iter])Punto2(tst2->GetPhi(), tst2->GetZ(), tst2->Getnum());
 						new(ptrhitsgood3[iter])Punto2(tst3->GetPhi(), tst3->GetZ(), tst3->Getnum());
 						iter++;
 						}
-					else continue;
-					
-				}
-				
-			}
+					//else continue;
+					}
+				   //else continue;
+				   }
+			       }
+			   //else continue;
+			   }
 			
 				
-			// __________tentativo ricostruzione vertice__________
-			// eq retta passante per due punti A(x2,y2,z2) e B(x3,y3,z3)
-			// x0 = x3 + c1t		c1 = sin(theta)*cos(phi)
-			// y0 = y3 + c2t		c2 = sin(theta)*sin(phi)
-			// z0 = z3 + c3t		c3 = cos(phi)
-			// noi vogliamo solo la z
-			// impongo y=0 e x=0, trovo quindi due valori di t e li medio tra loro (x3/c1 e y3/c2)
+	         // __________tentativo ricostruzione vertice__________
+		 // eq retta passante per due punti A(x2,y2,z2) e B(x3,y3,z3)
+		 // x0 = x3 + c1t		c1 = sin(theta)*cos(phi)
+		 // y0 = y3 + c2t		c2 = sin(theta)*sin(phi)
+		 // z0 = z3 + c3t		c3 = cos(phi)
+		 // noi vogliamo solo la z
+		 // impongo y=0 e x=0, trovo quindi due valori di t e li medio tra loro (x3/c1 e y3/c2)
 			
-				//cout<<" " <<ptrhitsgood2.GetEntries() << " " << ptrhitsgood3.GetEntries() <<endl;
-			for(int e=0; e<ptrhitsgood2.GetEntries(); e++)
-			{
-				Punto2 *tstgood2;
-				Punto2 *tstgood3;
-				tstgood2=(Punto2*)hitsgood2->At(e);
-				tstgood3=(Punto2*)hitsgood3->At(e);
 				
-				Phi2 = tstgood2->GetPhi();
-				X2 = R2*TMath::Cos(Phi2);
-				Y2 = R2*TMath::Sin(Phi2);
-				Z2 = tstgood2->GetZ();
+		 for(int e=0; e<ptrhitsgood2.GetEntries(); e++){
+			
+			Punto2 *tstgood2;
+			Punto2 *tstgood3;
+			tstgood2=(Punto2*)hitsgood2->At(e);
+			tstgood3=(Punto2*)hitsgood3->At(e);
 				
-				Phi3 = tstgood3->GetPhi();
-				X3 = R3*TMath::Cos(Phi3);
-				Y3 = R3*TMath::Sin(Phi3);
-				Z3 = tstgood3->GetZ();
+			Phi2 = tstgood2->GetPhi();
+			X2 = R2*TMath::Cos(Phi2);
+			Y2 = R2*TMath::Sin(Phi2);
+			Z2 = tstgood2->GetZ();
 				
+			Phi3 = tstgood3->GetPhi();
+			X3 = R3*TMath::Cos(Phi3);
+			Y3 = R3*TMath::Sin(Phi3);
+			Z3 = tstgood3->GetZ();
+																
+			//calcolo Phi e Theta della retta passante tra i due punti
+		        if(X3 < X2){
+			    Phi = TMath::ATan((Y3-Y2)/(X3-X2)) + TMath::Pi();
+			    }
+			else Phi = TMath::ATan((Y3-Y2)/(X3-X2));
 								
-				//cout<<" label 2 "<<tstgood2->Getnum()<<"  "<<"label 3 "<<tstgood3->Getnum()<<endl;
-			        //calcolo Phi e Theta della retta passante tra i due punti
-				 if(X3 < X2){
-					Phi = TMath::ATan((Y3-Y2)/(X3-X2)) + TMath::Pi();
-				}
-				else Phi = TMath::ATan((Y3-Y2)/(X3-X2));
+			if(Z3 < Z2){
+			    Theta = TMath::ATan((R3-R2)/(Z3-Z2)) + TMath::Pi();
+			    }
+			else Theta = TMath::ATan((R3-R2)/(Z3-Z2));
 				
-				
-				 if(Z3 < Z2){
-					Theta = TMath::ATan((R3-R2)/(Z3-Z2)) + TMath::Pi();
-				}
-				else Theta = TMath::ATan((R3-R2)/(Z3-Z2));
-				
-				 C[0] =  TMath::Sin(Theta)*TMath::Cos(Phi);		
-				 C[1] =  TMath::Sin(Theta)*TMath::Sin(Phi);		        
-				 C[2] =  TMath::Cos(Theta);	
+		        C[0] =  TMath::Sin(Theta)*TMath::Cos(Phi);		
+			C[1] =  TMath::Sin(Theta)*TMath::Sin(Phi);		        
+			C[2] =  TMath::Cos(Theta);	
 			
-				 t = (X3/C[0] + Y3/C[1])/2;
+		        t = (X3/C[0] + Y3/C[1])/2;
 				 
-				 //l'intersezione con la beam line sarà il mio Zrec
-				 Zrec = Z3 - C[2]*t;
+		        //l'intersezione con la beam line sarà il mio Zrec2
+			Zrec2 = Z3 - C[2]*t;
 				 
-				 hist2 -> Fill(Zrec);
-				
+			hist2 -> Fill(Zrec2);				
 			}
-
-			
-			Zvec[ev] = hist2->GetMean(); //salvo la media di tutto il mio istogramma, di tutte le Zrec (anche quelle derivate da coppie sbagliate) nel vector Zvec (solo una per evento)	
-                
-		
+						
+		Zrec[ev] = hist2->GetMean(); //salvo la media di tutto il mio istogramma, di tutte le Zrec2 (anche quelle derivate da coppie sbagliate) nel vector Zrec (solo una per evento)	
+        		
                 hist2->Reset("ICESM");
                 ptrhitsgood2.Clear();
-				ptrhitsgood3.Clear();
+		ptrhitsgood3.Clear();
 		}
 	
 		
-		delete hist2;
-					 
-		 hfile2.Close();
+	delete hist2;
+        hfile2.Close();
 			
-		 //creo grafico Ztrue-Zrec, avrò numeroeventi dati
-		 TCanvas *c1=new TCanvas("c1","c1",800,600);
-		 TH1D* hist1 = new TH1D ("h2","Ztrue-Zrec" , 100, -1, 1);	
+	//creo grafico Ztrue-Zrec, avrò numeroeventi dati
+	TCanvas *c1=new TCanvas("c1","c1",800,600);
+        TH1D* hist1 = new TH1D ("h2","Ztrue-Zrec" , 100, -1, 1);	
 	         		 
-		 for(int u = 0; u < numeroeventi; u++){
+	for(int u = 0; u < numeroeventi; u++){
 		 
-		 Zsim1 = Zsim[u];
-		 Zrec1 = Zvec[u];
+	      Zsim1 = Zsim[u];
+	      Zrec1 = Zrec[u];
 		 
-		 differenza = Zsim1 - Zrec1;
+	      differenza = Zsim1 - Zrec1;
 		 
-		 hist1 -> Fill(differenza); //grafico la differenza
-		 }
-		 
-		
-		 
-		hist1->Draw();
-		TFile file1("ztrue-zrec-tree.root", "recreate");
-		hist1->Write();
-		file1.Close();
-		
-		
-		
-  double TT = time.CpuTime();	
-  cout<<"Il tempo impiegato dalla CPU è "<<TT<<" s"<<endl;
+	      hist1->Fill(differenza); //grafico la differenza
+	      }
+		 		 
+	 hist1->Draw();
+	 
+	 TFile file1("ztrue-zrec-tree.root", "recreate");
+	 hist1->Write();
+	 file1.Close();
+				
+         double TT = time.CpuTime();	
+         cout<<"Il tempo impiegato dalla CPU è "<<TT<<" s"<<endl;
   
- }
- 
+         }
  
--- Simulazione_detector/Simulazione2.C
+++ Simulazione_detector/Simulazione2.C
@@ -3,35 +3,30 @@
 #include <iostream>
 #include <fstream>
 #include <math.h>
+#include <vector>
 
 #include "TFile.h"
 #include "TTree.h"
 #include "TBranch.h"
-
-#include "Punto.h"
-#include "Punto2.h"
 #include "TClonesArray.h"
 #include "Riostream.h"
 #include "TRandom3.h"
+
+#include "Punto2.h"
 #include "Vertex.h"
 #include "TracciaMC.h"
 #include "Multis.h"
-#include "TRandom3.h"
-
-#include <vector>
+#include "Smearing.h"
 
 using namespace std;
 
-void Simulazione2(bool multipleScatt){
+void Simulazione2(bool multipleScatt){ // 0 = false, 1 = true
 	
-  TStopwatch time;
-	
- //bool multipleScatt = 1;		// 0 = false, 1 = true
- 
-  // ______________grafico 3d intersezioni_______________
-		
+        TStopwatch time;	
+	 
+        //creo Tree dove intendo salvare i miei dati
 	TFile hfile2("htree2.root","RECREATE");	  
-        TTree *tree2 = new TTree("T2","TTree con 2 branches");
+        TTree *tree2 = new TTree("T2","TTree con 3 branches");
         
         int numeroeventi = 1000;
         
@@ -44,108 +39,114 @@ void Simulazione2(bool multipleScatt){
         TClonesArray *ptrhits3 = new TClonesArray("Punto2",100);
         TClonesArray &hits3 = *ptrhits3;
         
-      
+        //definisco una struct che contiene l'informazione sulla posizione e la molteplicità del vertice
+		 
+        typedef struct{
+               float X,Y,Z;
+               int mult;} VTX;
+        static VTX point;
+		 
+        //creo i branches del tree
         
-        //ofstream fileout1("Zsim.txt"); //forse questo diventa inutile perchè ho già una foglia del tree con la Z del vertice
-		 
-		   typedef struct{
-                   float X,Y,Z;
-                   int mult;} VTX;
-    
-    
-                   static VTX point;
-		 
-		   tree2->Branch("VertMult",&point.X,"X/F:Y:Z:mult/I");
-                   tree2->Branch("HitsPrimo",&ptrhits1);
-                   tree2->Branch("HitsSecondo",&ptrhits2);
-                   tree2->Branch("HitsTerzo",&ptrhits3);
+        tree2->Branch("VertMult",&point.X,"X/F:Y:Z:mult/I");
+        tree2->Branch("HitsPrimo",&ptrhits1);
+        tree2->Branch("HitsSecondo",&ptrhits2);
+        tree2->Branch("HitsTerzo",&ptrhits3);
                    
+        //creo gli oggetti da usare per ogni evento della simulazione         
+        
+        std::array<float,2> intersez; //array contenente le coordinate delle intersezioni
                   
-                   std::array<float,2> intersez;
-                  
-                   Vertex vertice0;
-                   
+        Vertex vertice0; //vertice
+        Multis catter; //multiple scattering
+        
+        int numSpuri = 5; //numero di eventi spuri simulati per ogni evento
+        int numParticella; // label per identificare le tracce simulate
+        int count=0;
               
 	for(int evento = 0; evento < numeroeventi; evento++){
-	
-
-		    vertice0.NewVertex();
+	         //if(evento%50000==0) cout<<"siamo arrivati al numero "<<evento<<endl; //controllo su come procede la simulazione
+		 vertice0.NewVertex(); //estraggo nuove coordinate casuali del vertice
+		 	
+		 //salvo informazioni nell'oggetto point	 
+		 point.mult = vertice0.GetMolteplicity();
+                 point.X = vertice0.GetX();
+                 point.Y = vertice0.GetY();
+                 point.Z = vertice0.GetZ();
+		       		 		 
+		 //calcolo intersezioni su ogni layer		
+		 numParticella = 0;		
 		 		 
-		     point.mult = vertice0.GetMolteplicity();
-                     point.X = vertice0.GetX();
-                     point.Y = vertice0.GetY();
-                     point.Z = vertice0.GetZ();
-
-		       		 
-		  // __________creo oggetto Multis per il Multiple Scattering____________
-		  
-		  Multis catter;
-		 
-		 // __________calcolo intersezioni su ogni layer____________
-		
-		 int i = 0;
-		 int numParticella = 0;		// label per identificare le tracce simulate
-		 
-		 int numSpuri = 5;
-		 
 		 do{		 		
-			 TracciaMC tr(0.,0.,point.X,point.Y,point.Z);
+			 TracciaMC tr(0.,0.,point.X,point.Y,point.Z); //creo oggetto traccia inserendo le coordinate del Vertice
 			 
+			 //imposto la direzione iniziale della particella, estratta casualmente
 			 tr.SetDistribEta(); 
 			 tr.SetDistribPhi();
-			 tr.Theta();
-			 
+			 tr.Theta();			 
 			 tr.CalcCoeff();
 			 
-			 intersez = tr.intersezione(3);
-			 if(intersez[1] < -27/2 || intersez[1] > 27/2) continue;
-			 
+			 //controllo che la particella non esca dal terzo layer del rivelatore
+			/* intersez = tr.intersezione(3);
+			 if(intersez[1] < -27/2 || intersez[1] > 27/2) {
+			 count++;
+			 cout<<"andata fuori numero "<<count<<endl;
+			 continue;
+			 }
+			 */
 			 
 			 for(int t=0; t<3; t++){
 			 
-				intersez = tr.intersezione(t+1);										
-				catter.NuoviAngoli(multipleScatt);
-				catter.VarioAngolo(tr);
-				tr.SetOrigine(intersez, t+1);
+		          intersez = tr.intersezione(t+1); //calcolo intersezione con il layer
+		          
+		          if((intersez[1] >= -27/2) && (intersez[1] <= 27/2)){									
+			    
+			    catter.NuoviAngoli(multipleScatt); //estraggo nuovi angoli del multiple scattering casuali (se multipleScatt = 1)
+		            catter.VarioAngolo(tr); //conseguentemente cambiano gli angoli della direzione della particella 
+			    tr.SetOrigine(intersez, t+1); //l'intersezione è un punto della nuova retta descritta dalla particella
 				
-				Smearing ringo;
-				ringo.smearZ(intersez[1]);
-                                ringo.smearPhi(intersez[0], t+1);
+			    Smearing ringo; //estratti gaussianamente un dZ e dPhi che si andranno a sommare alle variabili vere
+			    ringo.smearZ(intersez[1]);
+                            ringo.smearPhi(intersez[0], t+1);
+                                
+                            //inserisco le hits dentro ai branches del tree   				
+			    if(t==0) new(hits1[numParticella])Punto2(ringo.GetPhirec(),ringo.GetZrec(), numParticella);
+			    if(t==1) new(hits2[numParticella])Punto2(ringo.GetPhirec(),ringo.GetZrec(), numParticella);
+			    if(t==2) new(hits3[numParticella])Punto2(ringo.GetPhirec(),ringo.GetZrec(), numParticella);
+			    }
+			  
+			    else{
+			    if(t==0) new(hits1[numParticella])Punto2(intersez[0],intersez[1], -1);
+			    if(t==1) new(hits2[numParticella])Punto2(intersez[0],intersez[1], -1);
+			    if(t==2) new(hits3[numParticella])Punto2(intersez[0],intersez[1], -1);
+			    }
 				
-				if(t==0) new(hits1[i])Punto2(ringo.GetPhirec(),ringo.GetZrec(), numParticella);
-				if(t==1) new(hits2[i])Punto2(ringo.GetPhirec(),ringo.GetZrec(), numParticella);
-				if(t==2) new(hits3[i])Punto2(ringo.GetPhirec(),ringo.GetZrec(), numParticella);
-				
-				}			
-			
-			 i++;
-			 numParticella++;
+			  }			
+		
+			  numParticella++;
 			 
-		 }while( i < point.mult);
+		   }while(numParticella < point.mult);
                
-                // aggiungo ora numSpuri punti spuri su ciascun layer
+                 // aggiungo ora numSpuri punti spuri su ciascun layer
 		 for(int u=0; u<numSpuri; u++){
 		    new(hits1[point.mult + u])Punto2(gRandom->Rndm()*2*TMath::Pi(),gRandom->Rndm()*27 - (27./2),-20);
 		    new(hits2[point.mult + u])Punto2(gRandom->Rndm()*2*TMath::Pi(),gRandom->Rndm()*27 - (27./2),-20);
 		    new(hits3[point.mult + u])Punto2(gRandom->Rndm()*2*TMath::Pi(),gRandom->Rndm()*27 - (27./2),-20);
-		 }
-		 tree2->Fill();
+		    }
+		    
+		 tree2->Fill(); //riempie il tree
+		 
+		 //ripristino i TClonesArrays
                  ptrhits1->Clear();
                  ptrhits2->Clear();
                  ptrhits3->Clear();
 		 }
-		
-		
-		 
-		 // Save all objects in this file
-                 hfile2.Write();
-
-                 // Close the file. 
+				 
+		 //salvo il tree sul file e lo chiudo
+                 hfile2.Write();              
                  hfile2.Close();
                  
-                 
-                 
-                  double TT = time.CpuTime();	
-  cout<<"Il tempo impiegato dalla CPU è "<<TT<<" s"<<endl;
+                 double TT = time.CpuTime();	
+                 cout<<"Il tempo impiegato dalla CPU è "<<TT<<" s"<<endl;
 	
  }
